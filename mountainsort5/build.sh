4c359064ef82347131b247691ff4575cad49e5e0
Vincent <prevosto@mit.edu>
2023-04-19 11:10:19 -0400

    for local tests

--- mountainsort5/build.sh
+++ mountainsort5/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t spikeinterface/mountainsort5-base:latest -t spikeinterface/mountainsort5-base:1.0.0 .
+# docker build -t spikeinterface/mountainsort5-base:latest -t spikeinterface/mountainsort5-base:1.0.0 .
+docker build -t wanglabneuro/mountainsort5-base:latest -t wanglabneuro/mountainsort5-base:1.0.0 .
--- mountainsort5/push.sh
+++ mountainsort5/push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker push --all-tags spikeinterface/mountainsort5-base
+# docker push --all-tags spikeinterface/mountainsort5-base
+docker push --all-tags wanglabneuro/mountainsort5-base
